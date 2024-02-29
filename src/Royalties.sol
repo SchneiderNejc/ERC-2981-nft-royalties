49fb35e05265f2e8daa0429defa9ce5f59146313
SchneiderNejc <nejc.sch@gmail.com>
2024-02-29 20:04:01 +0100

    import OZ

--- src/Royalties.sol
+++ src/Royalties.sol
@@ -1,5 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/common/ERC2981.sol";
+
 contract Royalties {
 
 }
