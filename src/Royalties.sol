318201521c2c21ef7b9dd00293417364acee2699
SchneiderNejc <nejc.sch@gmail.com>
2024-02-29 21:10:28 +0100

    make contract Ownable

--- src/Royalties.sol
+++ src/Royalties.sol
@@ -3,11 +3,13 @@ pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/common/ERC2981.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Royalties {
 
-}    constructor(string memory _name, string memory _symbol, uint96 _feeNumerator) 
+contract Royalties is Ownable {
+    constructor(string memory _name, string memory _symbol, uint96 _feeNumerator) 
         ERC712(_name, _symbol) 
     {
         _setDefaultRoyalty(msg.sender, _feeNumerator);
     }
+}
