bb7f4efd824a4a27cf97c9c93d220cb5f647d8ae
SchneiderNejc <nejc.sch@gmail.com>
2024-02-29 21:09:08 +0100

    add constructor

--- src/Royalties.sol
+++ src/Royalties.sol
@@ -6,4 +6,8 @@ import "@openzeppelin/contracts/token/common/ERC2981.sol";
 
 contract Royalties {
 
-}
+}    constructor(string memory _name, string memory _symbol, uint96 _feeNumerator) 
+        ERC712(_name, _symbol) 
+    {
+        _setDefaultRoyalty(msg.sender, _feeNumerator);
+    }
