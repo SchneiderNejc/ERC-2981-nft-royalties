c097bd873093c0f155449a56ee348debe60ab5f0
SchneiderNejc <nejc.sch@gmail.com>
2024-03-01 16:44:38 +0100

    update inheritance

--- src/Royalties.sol
+++ src/Royalties.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/common/ERC2981.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 
-contract Royalties is Ownable {
+contract Royalties is ERC721URIStorage, ERC2981, Ownable {
     uint _tokenIdsTracker;
     constructor(string memory _name, string memory _symbol, uint96 _feeNumerator) 
         ERC721(_name, _symbol) 
