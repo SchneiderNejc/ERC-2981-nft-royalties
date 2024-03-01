474dd542251ecbc51ad91b4201d78e3724609d94
SchneiderNejc <nejc.sch@gmail.com>
2024-03-01 16:43:39 +0100

    remove Counters as they are depricated in solc 0.8.x

--- src/Royalties.sol
+++ src/Royalties.sol
@@ -3,13 +3,11 @@ pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/common/ERC2981.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 
 contract Royalties is Ownable {
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIdsTracker;
+    uint _tokenIdsTracker;
     constructor(string memory _name, string memory _symbol, uint96 _feeNumerator) 
         ERC712(_name, _symbol) 
     {
@@ -21,8 +19,8 @@ contract Royalties is Ownable {
         onlyOwner
         returns (uint256) 
     {
-        _tokenIdsTracker.increment();
-        uint tokenId = _tokenIdsTracker.current();
+        _tokenIdsTracker++;
+        uint tokenId = _tokenIdsTracker;
         _safeMint(recipient, tokenId);
         _setTokenURI(tokenId, tokenURI);
         return tokenId;
