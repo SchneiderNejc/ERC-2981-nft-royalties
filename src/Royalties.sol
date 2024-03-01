f66dd930308fdc56f6336dcc093a2e17db53de17
SchneiderNejc <nejc.sch@gmail.com>
2024-03-01 13:30:14 +0100

    init counters

--- src/Royalties.sol
+++ src/Royalties.sol
@@ -3,10 +3,13 @@ pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/common/ERC2981.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 
 contract Royalties is Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIdsTracker;
     constructor(string memory _name, string memory _symbol, uint96 _feeNumerator) 
         ERC712(_name, _symbol) 
     {
